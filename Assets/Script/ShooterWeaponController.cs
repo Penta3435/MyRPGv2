dec6afa51f2c5b97a4b12b6edecebdd896ef0fe4
Penta3435 <pentakill3435@gmail.com>
2023-06-04 23:17:19 +0200

    haciendo habilidades

--- Assets/Script/ShooterWeaponController.cs
+++ Assets/Script/ShooterWeaponController.cs
@@ -3,97 +3,111 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class ShooterWeaponController : WeaponController
-{
-    [Header("Default Weapon")]
-    [SerializeField] GameObject weapon2;
+//public class ShooterWeaponController : WeaponController
+//{
+//    [Header("Default Weapon")]
+//    [SerializeField] GameObject weapon2;
 
-    public override void EquipWeapon(GameObject weapon)
-    {
-        Weapon weaponComponent = weapon.GetComponent<Weapon>();
-        if (weaponComponent.WeaponType == WeaponType.ShooterRange)
-        {
-            if (weapon2.activeSelf == true)
-            {
-                UnequipWeapon(WeaponContainer.Weapon2Container);
-                weapon2 = Instantiate(weapon, hand);
-            }
-            else
-            {
-                UnequipWeapon(WeaponContainer.Weapon1Container);
-                weapon1 = Instantiate(weapon, hand);
-            }
-        }
-        else if (weaponComponent.WeaponType == WeaponType.ShooterMelee)
-        {
-            UnequipWeapon(WeaponContainer.MeleeWeaponContainer);
-            meleeWeapon = Instantiate(weapon, hand);
-        }
-    }
-    public override void UnequipWeapon(WeaponContainer weaponContainer)
-    {
-        switch (weaponContainer)
-        {
-            case WeaponContainer.Weapon1Container:
-                if (weapon1.activeSelf == true) UseWeapon(WeaponContainer.MeleeWeaponContainer);
-                Destroy(weapon1);
-                weapon1 = null;
-                break;
+//    protected override void EquipWeapon(GameObject weapon)
+//    {
+//        Weapon weaponComponent = weapon.GetComponent<Weapon>();
+//        if (weaponComponent.WeaponType == WeaponType.ShooterRange)
+//        {
+//            if(weapon1 == null)
+//            {
+//                weapon1 = Instantiate(weapon, hand);
+//            }
+//            else if (weapon2 == null)
+//            {
+//                weapon2 = Instantiate(weapon, hand);
+//            }
+//            else if (weapon2.activeSelf == true)
+//            {
+//                UnequipWeapon(WeaponContainer.Weapon2Container);
+//                weapon2 = Instantiate(weapon, hand);
+//            }
+//            else
+//            {
+//                UnequipWeapon(WeaponContainer.Weapon1Container);
+//                weapon1 = Instantiate(weapon, hand);
+//            }
+//        }
+//        else if (weaponComponent.WeaponType == WeaponType.ShooterMelee)
+//        {
+//            UnequipWeapon(WeaponContainer.MeleeWeaponContainer);
+//            meleeWeapon = Instantiate(weapon, hand);
+//        }
+//    }
+//    public override void UnequipWeapon(WeaponContainer weaponContainer)
+//    {
+//        switch (weaponContainer)
+//        {
+//            case WeaponContainer.Weapon1Container:
+//                if (weapon1 == null) return;
+//                if (weapon1.activeSelf == true) UseWeapon(WeaponContainer.MeleeWeaponContainer);
+//                Destroy(weapon1);
+//                weapon1 = null;
+//                break;
 
-            case WeaponContainer.Weapon2Container:
-                if (weapon2.activeSelf == true) UseWeapon(WeaponContainer.MeleeWeaponContainer);
-                Destroy(weapon2);
-                weapon2 = null;
-                break;
-            default:
-                Destroy(meleeWeapon);
-                meleeWeapon = null;
-                break;
-        }
-    }
-    public override void SwitchWeapon()
-    {
-        if(weapon1 != null && weapon2 != null)
-        {
-            if(weapon1.activeSelf == true)
-            {
-                UseWeapon(WeaponContainer.Weapon2Container);
-            }
-            else 
-            {
-                UseWeapon(WeaponContainer.Weapon1Container);
-            }
-        }
-        else if(weapon1 != null ||  weapon2 != null)
-        {
-            if (meleeWeapon.activeSelf == true)
-            {
-                if (weapon1 != null) UseWeapon(WeaponContainer.Weapon1Container);
-                else UseWeapon(WeaponContainer.Weapon2Container);
-            }
-            else
-                UseWeapon(WeaponContainer.MeleeWeaponContainer);
-        }
-    }
-    protected override void UseWeapon(WeaponContainer weaponContainer)
-    {
-        if (weaponContainer == WeaponContainer.Weapon1Container && weapon1 == null)
-        {
-            weapon1.SetActive(true);
-            weapon2.SetActive(false);
-            meleeWeapon.SetActive(false);
-        }
-        else if(weaponContainer == WeaponContainer.Weapon2Container && weapon2 == null)
-        {
-            weapon1.SetActive(false);
-            weapon2.SetActive(true);
-            meleeWeapon.SetActive(false);
-        }
-        else
-        {
-            weapon1.SetActive(false);
-            weapon2.SetActive(false);
-            meleeWeapon.SetActive(true);
-        }
-    }
-}
+//            case WeaponContainer.Weapon2Container:
+//                if (weapon2 == null) return;
+//                if (weapon2.activeSelf == true) UseWeapon(WeaponContainer.MeleeWeaponContainer);
+//                Destroy(weapon2);
+//                weapon2 = null;
+//                break;
+//            default:
+//                if (meleeWeapon == null) return;
+//                Destroy(meleeWeapon);
+//                meleeWeapon = null;
+//                break;
+//        }
+//    }
+//    public override void SwitchWeapon()
+//    {
+//        if(weapon1 != null && weapon2 != null)
+//        {
+//            if(weapon1.activeSelf == true)
+//            {
+//                UseWeapon(WeaponContainer.Weapon2Container);
+//            }
+//            else 
+//            {
+//                UseWeapon(WeaponContainer.Weapon1Container);
+//            }
+//        }
+//        else if(weapon1 != null ||  weapon2 != null)
+//        {
+//            if (meleeWeapon.activeSelf == true)
+//            {
+//                if (weapon1 != null) UseWeapon(WeaponContainer.Weapon1Container);
+//                else UseWeapon(WeaponContainer.Weapon2Container);
+//            }
+//            else
+//                UseWeapon(WeaponContainer.MeleeWeaponContainer);
+//        }
+//    }
+//    protected override void UseWeapon(WeaponContainer weaponContainer)
+//    {
+//        if (weaponContainer == WeaponContainer.Weapon1Container && weapon1 == null)
+//        {
+//            weapon1.SetActive(true);
+//            weapon2.SetActive(false);
+//            meleeWeapon.SetActive(false);
+//            animator.runtimeAnimatorController = weapon1.GetComponent<Weapon>().AnimatorController;
+//        }
+//        else if(weaponContainer == WeaponContainer.Weapon2Container && weapon2 == null)
+//        {
+//            weapon1.SetActive(false);
+//            weapon2.SetActive(true);
+//            meleeWeapon.SetActive(false);
+//            animator.runtimeAnimatorController = weapon2.GetComponent<Weapon>().AnimatorController;
+//        }
+//        else
+//        {
+//            weapon1.SetActive(false);
+//            weapon2.SetActive(false);
+//            meleeWeapon.SetActive(true);
+//            animator.runtimeAnimatorController = meleeWeapon.GetComponent<Weapon>().AnimatorController;
+//        }
+//    }
+//}
--- Assets/Script/WeaponController.cs
+++ Assets/Script/WeaponController.cs
@@ -8,84 +8,158 @@ public class WeaponController : MonoBehaviour
     [SerializeField] protected PlayerInput playerInput;
     [SerializeField] protected Animator animator;
     [SerializeField] protected Transform hand;
+    [SerializeField] protected WeaponType[] canEquipWeaponType;
 
     [Header("Default Weapon")]
-    [SerializeField] protected GameObject weapon1;
+    [SerializeField] protected GameObject[] weapons;
     [SerializeField] protected GameObject meleeWeapon;
-    public void Atack(InputAction.CallbackContext ctx)
+
+    Stack<GameObject> weaponsStack;
+
+    protected void Awake()
+    {
+        weaponsStack = new Stack<GameObject>(weapons);
+    }
+
+
+
+    public virtual void Atack(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
-            PlayAtackAnimation();
+            animator.SetTrigger("Atack");
     }
-    public virtual void EquipWeapon(GameObject weapon)
+    protected virtual void EquipWeapon(GameObject weapon)
     {
-        Weapon weaponComponent = weapon.GetComponent<Weapon>();
-        if(weaponComponent.WeaponType == WeaponType.TankWeapon)
+        bool canEquip = false;
+        foreach(var weaponType in canEquipWeaponType)
         {
-            UnequipWeapon(WeaponContainer.Weapon1Container);
-            weapon1 = Instantiate(weapon, hand);
-            UseWeapon(WeaponContainer.Weapon1Container);
+            if (weapon.GetComponent<Weapon>().WeaponType == weaponType) 
+            { 
+                canEquip = true;
+                break;
+            }
         }
-        else if(weaponComponent.WeaponType == WeaponType.TankMelee)
+        if (!canEquip) return;
+
+
+        if(weaponsStack.Count != weapons.Length)
         {
-            UnequipWeapon(WeaponContainer.MeleeWeaponContainer);
-            meleeWeapon = Instantiate(weapon, hand);
+            for (int weaponInt = 0; weaponInt < weapons.Length; weaponInt++)
+            {
+                if (weapons[weaponInt] == null)
+                {
+                    weapons[weaponInt] = weapon;
+                    weaponsStack.Push(weapon);
+                    UseWeapon(weaponInt);
+                    return;
+                }
+            }
         }
+        else
+        {
+            for (int weaponInt = 0; weaponInt < weapons.Length; weaponInt++)
+            {
+                if (weapons[weaponInt].activeSelf == true)
+                {
+                    UnequipWeapon(weaponInt);
+                    weapons[weaponInt] = weapon;
+                    weaponsStack.Push(weapon);
+                    UseWeapon(weaponInt);
+                    return;
+                }
+                else
+                {
+                    UnequipWeapon(weaponInt);
+                    weapons[weaponInt] = weapon;
+                    weaponsStack.Push(weapon);
+                    UseWeapon(weaponInt);
+                    return;
+                }
+            }
+        }
+    //    Weapon weaponComponent = weapon.GetComponent<Weapon>();
+    //    if(weaponComponent.WeaponType == WeaponType.TankWeapon)
+    //    {
+    //        UnequipWeapon(WeaponContainer.Weapon1Container);
+    //        weapon1 = Instantiate(weapon, hand);
+    //        UseWeapon(WeaponContainer.Weapon1Container);
+    //    }
+    //    else if(weaponComponent.WeaponType == WeaponType.TankMelee)
+    //    {
+    //        UnequipWeapon(WeaponContainer.MeleeWeaponContainer);
+    //        meleeWeapon = Instantiate(weapon, hand);
+    //    }
     }
     public virtual void UnequipWeapon(WeaponContainer weaponContainer)
     {
-        if(weaponContainer == WeaponContainer.Weapon1Container)
+        if (weaponContainer == WeaponContainer.Weapon1Container)
         {
-            Destroy(weapon1);
-            weapon1 = null;
-            meleeWeapon.SetActive(true);
+            if (weapons[0].activeSelf == true) meleeWeapon.SetActive(true);
+            Destroy(weapons[0]);
+            weapons[0] = null;
+            weaponsStack = new Stack<GameObject>(weapons);
         }
         else
         {
             Destroy(meleeWeapon);
             meleeWeapon = null;
+            weaponsStack = new Stack<GameObject>(weapons);
         }
     }
-    public virtual void SwitchWeapon()
+    public virtual void UnequipWeapon(int weaponInt)
     {
-        if (weapon1 == null) return;
-
-        if (meleeWeapon.activeSelf == true)
-        {
-            UseWeapon(WeaponContainer.Weapon1Container);
-        }
-        else
-        {
-            UseWeapon(WeaponContainer.MeleeWeaponContainer);
-        }
+        if (weapons[weaponInt].activeSelf == true) meleeWeapon.SetActive(true); 
+        Destroy(weapons[weaponInt]);
+        weapons[weaponInt] = null;
+        weaponsStack = new Stack<GameObject>(weapons);
     }
+    //public virtual void SwitchWeapon()
+    //{
+    //    if (weapon1 == null) return;
+
+    //    if (meleeWeapon.activeSelf == true)
+    //    {
+    //        UseWeapon(WeaponContainer.Weapon1Container);
+    //    }
+    //    else
+    //    {
+    //        UseWeapon(WeaponContainer.MeleeWeaponContainer);
+    //    }
+    //}
     public virtual void SwitchToMelee()
     {
         UseWeapon(WeaponContainer.MeleeWeaponContainer);
     }
     protected virtual void UseWeapon(WeaponContainer weaponContainer)
     {
-        if (weaponContainer == WeaponContainer.Weapon1Container && weapon1 == null)
+        if (weaponContainer == WeaponContainer.Weapon1Container && weapons[0] == null)
         {
             meleeWeapon.SetActive(false);
-            weapon1.SetActive(true);
+            weapons[0].SetActive(true);
+            animator.runtimeAnimatorController = weapons[0].GetComponent<Weapon>().AnimatorController;
         }
         else
         {
-            weapon1.SetActive(false);
             meleeWeapon.SetActive(true);
+            weapons[0].SetActive(false);
+            animator.runtimeAnimatorController = weapons[1].GetComponent<Weapon>().AnimatorController;
         }
     }
-    void PlayAtackAnimation()
+    protected virtual void UseWeapon(int weaponInt)
     {
-        animator.SetTrigger("Atack");
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("WeaponDrop") && playerInput.actions.FindActionMap("PlayerRunning").FindAction("EquipWeapon").WasPerformedThisFrame())
+        foreach(var weapon in weapons)
         {
-            print("WEQUIP");
-            EquipWeapon(other.GetComponent<WeaponDrop>().Weapon);
+            weapon.SetActive(false);
         }
+        weapons[weaponInt].SetActive(true);
+        animator.runtimeAnimatorController = weapons[weaponInt].GetComponent<Weapon>().AnimatorController;
     }
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.CompareTag("WeaponDrop") && playerInput.actions.FindActionMap("PlayerRunning").FindAction("EquipWeapon").WasPerformedThisFrame())
+    //    {
+    //        print("equip");
+    //        EquipWeapon(other.GetComponent<WeaponDrop>().Weapon);
+    //    }
+    //}
 }
